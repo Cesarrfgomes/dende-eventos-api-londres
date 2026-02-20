cc3523aa664b4e15d2394f2e048bc9899abdafac
Rodrigo Guima <rodrigo.souzag04@gmail.com>
2026-02-19 21:14:35 -0300

    Task 7

---
+++ src/main/java/br/com/softhouse/dende/model/Empresa.java
@@ -0,0 +1,36 @@
+package br.com.softhouse.dende.model;
+
+
+import java.util.Date;
+
+public class Empresa {
+
+    private String cnpj;
+    private String razaoSocial;
+    private String nomeFantasia;
+    private Date dataAbertura;
+
+    public Empresa(){
+
+    }
+
+    public Empresa(String cnpj, String razaoSocial, String nomeFantasia, Date dataAbertura){
+        this.cnpj = cnpj;
+        this.razaoSocial = razaoSocial;
+        this.nomeFantasia = nomeFantasia;
+        this.dataAbertura = dataAbertura;
+    }
+
+    public String getCnpj(){return cnpj;}
+    public void setCnpj(String cnpj){this.cnpj = cnpj;}
+
+    public String getRazaoSocial(){return razaoSocial;}
+    public void setRazaoSocial(String razaoSocial){this.razaoSocial = razaoSocial;}
+
+    public String getNomeFantasia(){return nomeFantasia;}
+    public void setNomeFantasia(String nomeFantasia){this.nomeFantasia = nomeFantasia;}
+
+    public Date getDataAbertura(){return dataAbertura;}
+    public void setDataAbertura(Date dataAbertura){this.dataAbertura = dataAbertura;}
+
+}
